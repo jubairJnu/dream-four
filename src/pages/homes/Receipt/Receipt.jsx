68ec715820cd1d75bbe3e34057e6dcafbbfcc1ad
jubairJnu <jubair2810@gmail.com>
2023-09-13 20:50:41 +0600

    added receipt and validation

--- src/pages/homes/Receipt/Receipt.jsx
+++ src/pages/homes/Receipt/Receipt.jsx
@@ -1,45 +1,52 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import Swal from "sweetalert2";
 
 const Receipt = () => {
-  const { register, handleSubmit,reset } = useForm();
+  const [services, setServices] = useState([]);
+  const [doctors, setDoctors] = useState([]);
+  const { register, handleSubmit,reset,formState:{errors}, watch } = useForm();
 
   const onSubmit = (data) =>{
-    Swal.fire({
-      title: 'Are you sure?',
-      text: "You cannot edit it!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire(
-          'Deleted!',
-          'Your file has been deleted.',
-          'success'
-        )
-      }
-    })
+    const {patient, phone,doctor,service,total,paid } = data;
+    const newReceipt = {patient, phone,doctor,service,total:parseFloat(total),paid:parseFloat(paid) }
+    console.log(newReceipt);
+    // Swal.fire({
+    //   title: 'Are you sure?',
+    //   text: "You cannot edit it!",
+    //   icon: 'warning',
+    //   showCancelButton: true,
+    //   confirmButtonColor: '#3085d6',
+    //   cancelButtonColor: '#d33',
+    //   confirmButtonText: 'Yes, delete it!'
+    // }).then((result) => {
+    //   if (result.isConfirmed) {
+    //     Swal.fire(
+    //       'Deleted!',
+    //       'Your file has been deleted.',
+    //       'success'
+    //     )
+    //   }
+    // })
   }
 
   return (
     <div className="container mx-auto mt-24">
       <h1 className="text-center font-bold text-2xl">Receipt Entry</h1>
-      <div className="hero min-h-screen px-10 ">
-        <div className="card flex-shrink-0 md:w-1/2 w-full max-w-sm shadow-2xl p-4 ">
+      <div className=" px-10 ">
+        <div className="   w-full max-w-md shadow-2xl p-4 ">
         <form onSubmit={handleSubmit(onSubmit)}>
             <div className="form-control">
               <label className="label">
                 <span className="label-text md:text-[18px] font-semibold">Patient Name * </span>
               </label>
               <input
-                {...register("name", { required: true })}
+                {...register("patient", { required: true, })}
                 type="text"
                 placeholder="Your Name"
                 className="input input-bordered input-primary"
               />
+              {errors.patient && <span className="text-red-500">this field is required</span>}
             </div>
 
             <div className="form-control">
@@ -47,37 +54,35 @@ const Receipt = () => {
                 <span className="label-text md:text-[18px] font-semibold">Phone Number*</span>
               </label>
               <input
-                {...register("email", { required: true })}
-                type="email"
-                placeholder="example@email.com "
+                {...register("phone", { required: true, })}
+                type="number"
+                placeholder="phone number"
                 className="input input-bordered input-primary "
               />
+              {errors.phone && <span className="text-red-500">this field is required</span>}
             </div>
 
             <div className="form-control">
               <label className="label">
                 <span className="label-text font-semibold">Doctor *</span>
               </label>
-              <select defaultValue="pick One" {...register("doctor", { required: true })} className="select select-bordered select-primary">
+              <select defaultValue="pick One" {...register("doctor",{required: false } )} className="select select-bordered select-primary">
               <option disabled >Pick One</option>
-              <option>Samad</option>
-              <option>Imran</option>
-              <option>Imu</option>
-              <option>Zakir</option>
-              <option>Hossain</option>
+              {
+                doctors?.map(doctor => <option key={doctor._id}> { doctor.name } </option>)
+              }
+            
             </select>
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text font-semibold">Service *</span>
               </label>
-              <select defaultValue="pick One" {...register("service", { required: true })} className="select select-bordered select-primary">
+              <select defaultValue="pick One" {...register("service",{required: false } )} className="select select-bordered select-primary">
               <option disabled >Pick One</option>
-              <option>Operation</option>
-              <option>Sizzore</option>
-              <option>Consultant</option>
-              <option>Medicine</option>
-              <option>Report show</option>
+              {
+                services?.map(service => <option key={service._id}> { service.name } </option>)
+              }
             </select>
             </div>
 
@@ -86,7 +91,7 @@ const Receipt = () => {
                 <span className="label-text md:text-[18px] font-semibold">Total Amount * </span>
               </label>
               <input
-                {...register("total", { required: true })}
+                {...register("total", { required: true,valueAsNumber:true })}
                 type="number"
                 placeholder="Your Name"
                 className="input input-bordered input-primary"
@@ -98,11 +103,16 @@ const Receipt = () => {
                 <span className="label-text md:text-[18px] font-semibold">Pay Now * </span>
               </label>
               <input
-                {...register("paid", { required: true })}
+                {...register("paid", { required: true, maxLength : 5 ,valueAsNumber:true, validate: data =>{
+                  if(watch('total') < data ){
+                    return "Paid amount is bigger than total"
+                  }
+                } })}
                 type="number"
                 placeholder="Enter Amount"
                 className="input input-bordered input-primary"
               />
+              <p className="text-red-500"> {errors.paid?.message} </p>
             </div>
            
             <div className="flex justify-center">
